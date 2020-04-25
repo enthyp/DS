77d89477f3590ccad898aa27763a6474dea1bbc0
Jakub Łanecki <jakub.lanecki@relia-sol.pl>
2020-04-25 22:07:18 +0200

    PanzerClient

--- grpc/client/src/main/kotlin/mpk/App.kt
+++ grpc/client/src/main/kotlin/mpk/App.kt
@@ -2,13 +2,13 @@ package mpk
 
 import com.google.protobuf.Empty
 import io.grpc.ManagedChannelBuilder
-import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.asExecutor
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
+import io.grpc.StatusException
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.runBlocking
 import java.io.Closeable
+import java.io.IOException
+import java.lang.NumberFormatException
+import java.text.ParseException
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
@@ -20,13 +20,17 @@ class MpkClient constructor(
     private val channel = channelBuilder.build()
     private val stub = MpkPublisherGrpcKt.MpkPublisherCoroutineStub(channel)
 
-    suspend fun getSchedule() = coroutineScope {
+    suspend fun getSchedule() = supervisorScope {
         val empty = Empty.newBuilder().build()
         val response = async { stub.getSchedule(empty) }
-        println("SCHEDULE: ${response.await()}")
+        try {
+            println("SCHEDULE: ${response.await()}")
+        } catch (e: StatusException) {
+            onCommunicationError(e.message)
+        }
     }
 
-    suspend fun subscribe(stopId: Int, lines: List<Mpk.Line>) = coroutineScope {
+    suspend fun subscribe(stopId: Int, lines: List<Mpk.Line>) = supervisorScope {
         val request = Mpk.NotifyRequest.newBuilder()
             .setClientId(clientId)
             .setStopId(stopId)
@@ -34,8 +38,12 @@ class MpkClient constructor(
             .addAllLines(lines)
             .build()
 
-        stub.subscribe(request).collect{ response ->
-            println("OBSERVED: $response")
+        try {
+            stub.subscribe(request).collect { response ->
+                println("OBSERVED: $response")
+            }
+        } catch (e: StatusException) {
+            onCommunicationError(e.message)
         }
     }
 
@@ -43,6 +51,8 @@ class MpkClient constructor(
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
     }
 }
+
+fun onCommunicationError(msg: String?) = println("Connection error: $msg")
 
 
 fun main(args: Array<String>) = runBlocking {
@@ -62,33 +72,77 @@ fun main(args: Array<String>) = runBlocking {
 }
 
 suspend fun cmd(client: MpkClient) {
-    val inputLines = generateSequence { readLine() }
-
-    inputLines.forEach { line ->
-        when(line) {
-            "get schedule" -> client.getSchedule()
-            else -> {
-                val parts = line.split(":").map { p -> p.trim() }
-                if (parts.size != 2) {
-                    println("Incorrect input!")
-                } else {
-                    val stopId = parts[0].toInt()
-                    val lines = parseLines(parts[1])
+    while (true) {
+        val line = readLine()
+        when (CmdState.of(line)) {
+            CmdState.INIT -> {}
+            CmdState.SCHEDULE -> client.getSchedule()
+            CmdState.LINES -> {
+                try {
+                    val (stopId, lines) = parseLines()
                     client.subscribe(stopId, lines)
+                } catch (e: IOException) {
+                    onInputError("")
+                } catch (e: ParseException) {
+                    e.message?.let { msg -> onInputError(msg) }
+                } catch (e: NumberFormatException) {
+                    onInputError("Integers required.")
                 }
+            }
+            CmdState.EXIT -> return
+            else -> onInputError("")
+        }
+    }
+}
+
+fun onInputError(msg: String) = println("Incorrect input! $msg")
+
+enum class CmdState {
+    INIT,
+    SCHEDULE,
+    LINES,
+    EXIT,
+    ERR;
+
+    companion object {
+        @JvmStatic
+        fun of(line: String?): CmdState {
+            return when (line) {
+                null -> INIT
+                "get schedule" -> SCHEDULE
+                "subscribe" -> LINES
+                "exit" -> EXIT
+                else -> ERR
             }
         }
     }
 }
 
-fun parseLines(lines: String): List<Mpk.Line> {
-    return lines.split(",").map { line ->
-        val parts = line.trim().split(" ").map { p -> p.trim() }
-        val number = parts[0].toInt()
-        val direction = parts[1].trim().toBoolean()
-        Mpk.Line.newBuilder()
-            .setDirection(direction)
-            .setNumber(number)
-            .build()
+data class Subscription(val stopId: Int, val lines: List<Mpk.Line>)
+
+fun parseLines(): Subscription {
+    print("Stop ID: ")
+    val stopId = readLine()?.trim()?.toInt()
+
+    print("Lines: ")
+    val lineStr = readLine()?.trim()?.split(",")?.map { p -> p.trim() }
+    val lines = lineStr?.mapNotNull { line ->
+        val parts = line.split(" ").map { p -> p.trim() }
+        if (parts.size == 2) {
+            val number = parts[0].toInt()
+            val direction = parts[1].toBoolean()
+            Mpk.Line.newBuilder()
+                .setDirection(direction)
+                .setNumber(number)
+                .build()
+        } else {
+            throw ParseException("Incorrect line description: $line", 0)
+        }
+    }
+
+    if (stopId != null && !lines.isNullOrEmpty()) {
+        return Subscription(stopId, lines)
+    } else {
+        throw ParseException("", 0)
     }
 }
